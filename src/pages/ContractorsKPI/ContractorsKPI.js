397bed2c39edbca95c5d4668548df67dabc39489
tuanvu9981 <tuanvutruonglacnhatsinh0801@gmail.com>
2022-06-11 00:37:35 +0700

    feat: add search function

--- src/pages/ContractorsKPI/ContractorsKPI.js
+++ src/pages/ContractorsKPI/ContractorsKPI.js
@@ -1,16 +1,31 @@
 import React, { useState } from 'react';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import { Typography } from '@mui/material';
+import { Typography, InputBase, Button } from '@mui/material';
 import { KPIMockData } from './KPIMockData';
 import DefaultKPICard from '../../components/KPICard/DefaultKPICard';
 import AddKPICard from '../../components/KPICard/AddKPICard';
 import NavBar from "../../components/NavBar/NavBar";
 import Footer from "../../components/Footer/footer";
+import SearchIcon from '@mui/icons-material/Search';
 
 const ContractorsKPI = () => {
 
     const [KPIData, setKPIData] = useState(KPIMockData);
+
+    const [searchName, setSearchName] = useState('');
+    const onChangeSearchName = (event) => {
+        if (event.target.value === '') {
+            setKPIData(KPIMockData);
+            setSearchName('');
+        } else {
+            setSearchName(event.target.value);
+            const newKPIArray = KPIData.filter(
+                element => element.name.includes(searchName) === true
+            );
+            setKPIData(newKPIArray);
+        }
+    }
 
     const onAddNewKPI = (newKPI) => {
         setKPIData([...KPIData, newKPI]);
@@ -25,43 +40,68 @@ const ContractorsKPI = () => {
     // }
 
     return (
-        <Box
-            sx={{
-                display: "flex",
-                flexDirection: "column",
-                backgroundColor: "white",
-                boxShadow: "none",
-                overflow: "visible",
-            }}
-        >
+        <>
             <NavBar />
-            <Typography
+            <Box
                 sx={{
-                    fontSize: "30px",
-                    textAlign: "center",
-                    marginTop: "30px"
+                    display: "flex",
+                    flexDirection: "column",
+                    backgroundColor: "white",
+                    boxShadow: "none",
+                    overflow: "visible",
+                    alignItems: "center"
                 }}
             >
-                DỰ ÁN LẮP ĐẶT HỆ THỐNG ĐIỆN CHO VINCOM HAI BÀ TRƯNG
-            </Typography>
+                <Typography
+                    sx={{
+                        fontSize: "30px",
+                        textAlign: "center",
+                        marginTop: "30px"
+                    }}
+                >
+                    DỰ ÁN LẮP ĐẶT HỆ THỐNG ĐIỆN CHO VINCOM HAI BÀ TRƯNG
+                </Typography>
 
-            <Box p={5}>
-                <Grid container spacing={6}>
-                    {KPIData.map((element) => (
-                        <Grid
-                            item xs={12} sm={6} md={4} lg={3} xl={2}
-                            key={element.id}
-                        >
-                            <DefaultKPICard data={element} />
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexDirection: "row",
+                        alignItems: "center",
+                        border: 0.5,
+                        width: "25%",
+                        height: "50px",
+                        borderRadius: "10px",
+                        marginTop: "1%"
+                    }}
+                >
+                    <InputBase
+                        placeholder='Tìm KPI theo tên'
+                        value={searchName}
+                        onChange={onChangeSearchName}
+                        sx={{ marginLeft: "2.5%", flex: 5 }}
+                    />
+
+                    <SearchIcon sx={{ flex: 1 }} />
+                </Box>
+
+                <Box p={5}>
+                    <Grid container spacing={6}>
+                        {KPIData.map((element) => (
+                            <Grid
+                                item xs={12} sm={6} md={4} lg={3} xl={2}
+                                key={element.id}
+                            >
+                                <DefaultKPICard data={element} />
+                            </Grid>
+                        ))}
+                        <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+                            <AddKPICard onAddNewKPI={onAddNewKPI} />
                         </Grid>
-                    ))}
-                    <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
-                        <AddKPICard onAddNewKPI={onAddNewKPI} />
                     </Grid>
-                </Grid>
+                </Box>
             </Box>
             <Footer />
-        </Box>
+        </>
     );
 }
 
