76ce18edca0e0456e5186bfa28167baf0da0e494
tuanvu9981 <tuanvutruonglacnhatsinh0801@gmail.com>
2022-07-11 08:51:06 +0700

    feat: update foreman & approve work

--- src/pages/ForemanManage/ForemanManage.js
+++ src/pages/ForemanManage/ForemanManage.js
@@ -1,52 +1,64 @@
 import React from 'react';
 import NavBar from "../../components/NavBar/NavBar";
 import './foreman.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import useWork from "../../hooks/useWork";
 import Footer from "../../components/Footer/footer";
+import { Typography } from "@mui/material"
 
 function Foreman() {
 
-  const {works} = useWork()
+  const { works } = useWork()
 
   return (
     <div className="foreman">
-      <NavBar/>
-      <div className="foreman-header">
-        <h1>DỰ ÁN LẮP ĐẶT HỆ THỐNG ĐIỆN CHO VINCOM HAI BÀ TRƯNG</h1>
+      <NavBar />
+      <div className="foreman-header" style={{ textAlign: "center" }}>
+        <Typography
+          variant='button'
+          fontWeight="bold"
+          color="text"
+          textTransform="none"
+          fontSize="25.5px"
+          sx={{
+            textAlign: "center"
+          }}
+        >
+          DỰ ÁN LẮP ĐẶT HỆ THỐNG ĐIỆN CHO VINCOM HAI BÀ TRƯNG
+        </Typography>
         <div className="work-btn">
         </div>
       </div>
       <div className="foreman-body">
         <table className="table">
           <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col" id="foreman-content1">Nội dung</th>
-            <th scope="col" id="foreman-content2">Hình thức</th>
-          </tr>
+            <tr>
+              <th scope="col">STT</th>
+              <th scope="col" id="foreman-content1">Nội dung</th>
+              <th scope="col" id="foreman-content2">Hình thức</th>
+            </tr>
           </thead>
           <tbody className="table-body">
-          {
-            works.map((work, index) => {
-              return <tr>
-                <th scope="row">{index}</th>
-                <td>
-                  <Link className='link-content' to={`/work-submit/${work.id}`}>{work.name}</Link>
-                </td>
-                <td>
-                  <button
-                    className={"btn " + (work.isSubmitted ? "btn-success" : "btn-danger") + " btn1"}>
-                    {work.isSubmitted ? "Đã duyệt" : "Đang chờ duyệt"}
-                  </button>
-                </td>
-              </tr>
-            })
-          }
+            {
+              works.map((work, index) => {
+                return <tr>
+                  <th scope="row">{index}</th>
+                  <td>
+                    <Link className='link-content' to={`/work-submit/${work.id}`}>{work.name}</Link>
+                  </td>
+                  <td>
+                    <button
+                      className={"btn " + (work.isSubmitted ? "btn-success" : "btn-danger") + " btn1"}>
+                      {work.isSubmitted ? "Đã duyệt" : "Đang chờ duyệt"}
+                    </button>
+                  </td>
+                </tr>
+              })
+            }
           </tbody>
         </table>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   )
 }
